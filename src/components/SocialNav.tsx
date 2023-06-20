011e1cdf74afde1435d4664ddfa1d6cf32687fac
Tatyana Nemchina <23angorka07@gmail.com>
2023-06-20 10:36:08 +0300

    fix: added visually-hidden

--- src/components/SocialNav.tsx
+++ src/components/SocialNav.tsx
@@ -8,26 +8,31 @@ export const SocialNav = () => {
             <ul>
                 <li>
                     <a href="https://github.com/TatyanaNem">
-                        <Icon iconId={'git'} viewBox={'0 0 30 35'}/>
+                        <span className='visually-hidden'>Link to github</span>
+                        <Icon iconId={'github'} viewBox={'0 0 30 35'}/>
                     </a>
                 </li>
                 <li>
                     <a href="https://www.linkedin.com/in/tatyana-nemchina-50b113256/">
+                        <span className='visually-hidden'>Link to linkedIn</span>
                         <Icon iconId={'linkedIn'} viewBox={'0 0 30 35'}/>
                     </a>
                 </li>
                 <li>
                     <a href="https://vk.com/angorka23">
+                        <span className='visually-hidden'>Link to Vkontakte</span>
                         <Icon iconId={'vk'} viewBox={'0 0 42 27'} />
                     </a>
                 </li>
                 <li>
                     <a href="https://t.me/TanyaNemchina">
+                        <span className='visually-hidden'>Link to Telegram</span>
                         <Icon iconId={'telegram'} viewBox={'0 0 20 20'}/>
                     </a>
                 </li>
                 <li>
                     <a href="https://wa.me/79811068473">
+                        <span className='visually-hidden'>Link to Whatsapp</span>
                         <Icon iconId={'whatsapp'} viewBox={'0 0 20 20'}/>
                     </a>
                 </li>
--- src/components/common/Icon.tsx
+++ src/components/common/Icon.tsx
@@ -10,7 +10,7 @@ type IconPropsType = {
 
 const Icon = (props: IconPropsType) => {
     return (
-        <svg width={props.width || "30"} height={props.height || "30"} viewBox={props.viewBox || "0 0 30 30"} fill="none" xmlns="http://www.w3.org/2000/svg">
+        <svg width={props.width || "30"} height={props.height || "30"} viewBox={props.viewBox || "0 0 30 30"} fill="none" xmlns="http://www.w3.org/2000/svg" role="img" aria-label={props.iconId}>
             <use xlinkHref={`${iconsSprite}#${props.iconId}`}></use>
         </svg>
     );
